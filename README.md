010777224fc87757b9da1b026440ceb75ad2b107
Abhishek Kumar <abhsihek_ku@yahoo.com>
2020-05-11 03:08:37 +0530

    AWS rekognition demo

---
+++ README.md
@@ -0,0 +1,29 @@
+## This example is Using AWS Rekognition service to identify images 
+## appearing in webcam.
+
+### How to 
+
+* Install python3 and dependencies.
+* Configure aws keys & secrect in ~/.aws directory.
+* Run below command.
+* Open webpage.
+* Click Capture Imgae to chose image taken by webcam.
+* Click Identify to check Image label.
+
+
+```
+$./run.sh
+
+```
+
+###  Dependencies.
+
+```
+python3
+boto3
+flask
+AWS-rekognition service.
+webrtc
+
+```
+
--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-
+import base64
 import boto3
-from flask import Flask, render_template 
+from flask import Flask, render_template, request,make_response
+import json
+
 
 app = Flask(__name__)
 
@@ -12,6 +14,38 @@ def home():
     return render_template("image.html")
 
 
+@app.route('/saveFile', methods=['POST']) 
+def upload_base64_file(): 
+    data = request.get_json()
+    #print(data)
+    if data is None:
+        print("No valid request body, json missing!")
+        return json.dumps({'success':False}), 200, {'ContentType':'application/json'} 
+    else:
+        b64_string = data['img']
+        # this method convert and save the base64 string to image
+        #print(b64_string)
+        res = convert_and_save(b64_string)
+        return json.dumps({'success':True,'result': res}), 200, {'ContentType':'application/json'} 
+
+def convert_and_save(imagestring):
+
+    with open("tmp/capture.base64", "w") as fh:
+        fh.write(imagestring)
+
+    with open("tmp/capture.png", "wb") as fh:
+        fh.write(base64.decodebytes(imagestring.encode()))
+
+    response=detect_labels_local_file("tmp/capture.png")
+    return response['Labels'][0]['Name']
+
+def detect_labels_local_file(photo):
+    client=boto3.client('rekognition')
+   
+    with open(photo, 'rb') as image:
+        response = client.detect_labels(Image={'Bytes': image.read()})
+        
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
